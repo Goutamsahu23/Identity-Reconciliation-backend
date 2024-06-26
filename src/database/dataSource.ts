8a6949e378f33d3634bd8943fd4ecaed6cee2515
Goutamsahu23 <goutamsahu602@gmail.com>
2024-06-26 23:00:23 +0530

    updated env data with vercel DB

--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -3,20 +3,23 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const host = process.env.RENDER_POSTGRES_HOST; 
+const host = process.env.POSTGRES_HOST; 
 const port = parseInt(process.env.RENDER_POSTGRES_PORT || '5432', 10); 
-const username = process.env.RENDER_POSTGRES_USERNAME; 
-const password = process.env.RENDER_POSTGRES_PASSWORD; 
-const database = process.env.RENDER_POSTGRES_DATABASE;
+const username = process.env.POSTGRES_USER; 
+const password = process.env.POSTGRES_PASSWORD; 
+const database = process.env.POSTGRES_DATABASE;
 
 export const AppDataSource = new DataSource({
-  type: "postgres",
+  type: 'postgres',
   host: host,
   port: port,
   username: username,
   password: password,
   database: database,
-  entities: ["src/entity/*.ts"],
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  entities: ['src/entity/*.ts'],
   synchronize: true,
   logging: true,
 });
